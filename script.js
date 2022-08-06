6a63a6014a31a70eceec9e003c465de490217189
hjali7 <88620768+hjali7@users.noreply.github.com>
2022-08-07 03:43:37 +0430

    complete header

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 let searchForm = document.querySelector('.search-form');
-
 document.querySelector('#search-btn').onclick = ()=>{
     searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+};
+
+let shoppingCart = document .querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = ()=>{
+    shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+    searchForm.classList.remove('active');
+};
+
+let loginForm = document .querySelector('.login-form');
+document.querySelector('#login-btn').onclick = ()=>{
+    loginForm.classList.toggle('active');
+    navBar.classList.remove('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
 }
+
+let navBar = document .querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = ()=>{
+    navBar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.addEventListener('scroll',()=>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navBar.classList.remove('active');
+})
